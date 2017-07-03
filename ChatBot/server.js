6464dd6e24322cb48e9252cf068fc45179e5ebc1
David <dsdardik@gmail.com>
2017-07-03 13:11:17 -0400

    Setting up webhook

--- ChatBot/server.js
+++ ChatBot/server.js
@@ -15,6 +15,17 @@ app.get('/', function (req, res) {
     res.send('Hello world');
 });
 
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === "penPalServer_Token") {
+            console.log("Validating webhook");
+            res.status(200).send(req.query['hub.challenge']);
+  } else {
+                console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
 app.listen(app.get('port'), function () {
     console.log("Status: Running");
 });
